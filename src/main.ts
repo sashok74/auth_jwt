5dcd120438f3f93e0708988b32eb21b27f0eb428
Aleksandr Makarenko <makarenko@planarchel.ru>
2022-12-25 20:47:00 +0500

    add express and dotenv

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { analytics } from './modules/analytics.js'
-
-const message = 'Hello node!'
-console.log(message)
-
-analytics('test')
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+
+declare var process: {
+  env: {
+    PORT: number,
+  }
+}
+
+dotenv.config();
+
+//const PORT = Number(process.env.PORT) || 3333;
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, '192.168.122.107', () => console.log(`start server at ${PORT}`));
+
